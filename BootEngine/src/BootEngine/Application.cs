6d17eae9cf7d1d9774f75561df56d19a3180a2c9
Lincoln Konig <lincolnkonig5@gmail.com>
2019-09-21 16:41:08 -0300

    Setup initial Application for engine

---
+++ BootEngine/src/BootEngine/Application.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BootEngine
+{
+	public partial class Application : IDisposable
+	{
+		#region Properties
+		private bool disposed = false;
+		#endregion
+
+		~Application()
+		{
+			Dispose(false);
+		}
+
+		#region Public Methods
+		public virtual void Run()
+		{
+			while (true)
+			{
+				//
+			}
+		}
+		#endregion
+
+		#region IDisposable Methods
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					// called via myClass.Dispose(). 
+					// OK to use any private object references
+				}
+				// Release unmanaged resources.
+				// Set large fields to null.                
+				disposed = true;
+			}
+		}
+		#endregion
+	}
+}
---
+++ Sandbox/src/SandboxApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BootEngine;
+
+namespace Sandbox
+{
+	public class SandboxApp : Application
+	{
+		public static void Main(string[] args)
+		{
+			var app = new SandboxApp();
+			app.Run();
+			app.Dispose();
+		}
+	}
+}
