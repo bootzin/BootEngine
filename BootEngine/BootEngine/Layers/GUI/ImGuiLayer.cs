5e8e972053924b894ff4304aa05048f1299c5a47
Lincoln Konig <lincolnkonig5@gmail.com>
2020-11-13 16:14:05 -0300

    Revert "change to .NET 5"
    This reverts commit 7cccc07b26a5dc3475948e42112a31f343c00921.

--- BootEngine/BootEngine/Layers/GUI/ImGuiLayer.cs
+++ BootEngine/BootEngine/Layers/GUI/ImGuiLayer.cs
@@ -23,12 +23,15 @@ namespace BootEngine.Layers.GUI
 #if DEBUG
 			using Profiler fullProfiler = new Profiler(GetType());
 #endif
-			gd = Application.App.Window.GraphicsDevice;
-			cl = Application.App.Window.GraphicsDevice.ResourceFactory.CreateCommandList();
+			var window = Application.App.Window;
+			var sdlWindow = window.SdlWindow;
 
-			Controller = new ImGuiController(gd, gd.SwapchainFramebuffer.OutputDescription, Application.App.Window);
+			gd = window.GraphicsDevice;
+			cl = window.GraphicsDevice.ResourceFactory.CreateCommandList();
 
-			Application.App.Window.SdlWindow.Resized += () => Controller.WindowResized(Width, Height, Application.App.Window.SdlWindow.WindowState == WindowState.Minimized);
+			Controller = new ImGuiController(gd, gd.SwapchainFramebuffer.OutputDescription, window);
+
+			sdlWindow.Resized += () => Controller.WindowResized(Width, Height, sdlWindow.WindowState == WindowState.Minimized);
 		}
 
 		public override void OnDetach()
